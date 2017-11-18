6cfc0530ae3bb021aa27b8ff9552accfb0d151ec
tsepes.vlad <tsepes.vlad@hotmail.com>
2017-11-18 19:12:52 +0300

    Pronunciation

--- Services/GooglePronunciationService/Program.cs
+++ Services/GooglePronunciationService/Program.cs
@@ -4,14 +4,17 @@ namespace GooglePronunciationService
 {
     public static class GooglePronunciationService
     {
-        public static string UrlPartOne = "https://translate.google.com.vn/translate_tts?ie=UTF-8&q=";
-        public static string UrlPartTwo = "&client=tw-ob";
+        private static string UrlPartOne = "https://translate.google.com.vn/translate_tts?ie=UTF-8&q=";
+        private static string UrlPartTwo = "&tl=";
+        private static string UrlPartThree = "&client=tw-ob";
+        private static string DownLoadedFileName = "Pronunciation.mp3";
 
-        public static string GetPronunciation(string PronunciationWord, string PronunciationLanguage)
+        public static void GetPronunciation(string PronunciationWord, string PronunciationLanguage)
         {
-            WebClient PronunciationWebClient = new WebClient();
+            string DownloadUrl = UrlPartOne + WebUtility.UrlEncode(PronunciationWord) + UrlPartTwo + PronunciationLanguage + UrlPartThree;
 
-            return null;
+            WebClient PronunciationWebClient = new WebClient();
+            PronunciationWebClient.DownloadFile(DownloadUrl, DownLoadedFileName);
         }
     }
 }
